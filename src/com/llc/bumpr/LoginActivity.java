d9b90ea35c45fecf7ce00e2793839829a08bfb5b
cooper3330 <kyle.cooper@uky.edu>
2013-09-25 20:37:49 -0400

    -- Updated Search Drivers to check for working version of Google Play Services -- This is required for Maps V2.  With this, we can not add in map view.

--- src/com/llc/bumpr/LoginActivity.java
+++ src/com/llc/bumpr/LoginActivity.java
@@ -43,8 +43,8 @@ public class LoginActivity extends Activity {
              }
         });
     }
-    
-    @Override
+
+	@Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
--- src/com/llc/bumpr/SearchDrivers.java
+++ src/com/llc/bumpr/SearchDrivers.java
@@ -1,20 +1,39 @@
 package com.llc.bumpr;
 
 import com.example.bumpr.R;
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.common.ConnectionResult;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
+import android.widget.Toast;
 
 public class SearchDrivers extends Activity {
+
+	private static final int RQS_GooglePlayServices = 1;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		// TODO Auto-generated method stub
 		super.onCreate(savedInstanceState);
 	}
+	
+    @Override
+	protected void onResume() {
+		// TODO Auto-generated method stub
+		super.onResume();
+		
+		//Verify user has good version of google play services. Necessary for maps
+		int retCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable
+				(getApplicationContext());
+		//If successful, carry on.  Otherwise, request them to download GP Services
+		if (retCode != ConnectionResult.SUCCESS)
+			GooglePlayServicesUtil.getErrorDialog(retCode, this, RQS_GooglePlayServices).show();
+	}
 
+    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
