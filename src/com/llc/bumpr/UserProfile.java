7fe47c13b5eddfbd9f85438845e503080f3e622b
cooper3330 <kyle.cooper@uky.edu>
2013-09-29 21:03:09 -0400

    -- New implementation of circle image.  Need to figure out how to test it

--- src/com/llc/bumpr/UserProfile.java
+++ src/com/llc/bumpr/UserProfile.java
@@ -1,7 +1,9 @@
 package com.llc.bumpr;
 
 import android.app.Activity;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class UserProfile extends Activity {
 
@@ -11,6 +13,9 @@ public class UserProfile extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.user_profile);
+		ImageView profPic = (ImageView) findViewById(R.id.profile_pic);
+		GraphicsUtil imageHelper = new GraphicsUtil();
+		profPic.setImageBitmap(imageHelper.getCircleBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.ic_launcher), 256));
 	}
 
 }
