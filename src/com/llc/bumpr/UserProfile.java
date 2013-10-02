d2aa6f28080b1f60a5c160fdf6a4d1403546acfc
cooper3330 <kyle.cooper@uky.edu>
2013-10-01 22:37:58 -0400

    -- Updated User Profile Page.  Still trying to figure out profile picture centering

--- src/com/llc/bumpr/UserProfile.java
+++ src/com/llc/bumpr/UserProfile.java
@@ -4,7 +4,10 @@ import android.app.Activity;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.view.ViewGroup.MarginLayoutParams;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 public class UserProfile extends Activity {
 
@@ -14,11 +17,27 @@ public class UserProfile extends Activity {
 		super.onCreate(savedInstanceState);
 		
 		setContentView(R.layout.user_profile);
-		ImageView profPic = (ImageView) findViewById(R.id.profile_pic);
+		
+		//Retrieve views
+		ImageView profPic = (ImageView) findViewById(R.id.iv_profile_pic);
+		ImageView carImg = (ImageView) findViewById(R.id.iv_car_image);
+		TextView userName = (TextView) findViewById(R.id.tv_user_name);
+		
+		//Set images
+		carImg.setImageResource(R.drawable.test_car_image);
 		GraphicsUtil imageHelper = new GraphicsUtil();
 		Bitmap bm = imageHelper.getCircleBitmap(BitmapFactory.decodeResource(getResources(), R.drawable.test_image), 16);
 		//Resize image to the desired size
 		Bitmap resizedBM = Bitmap.createScaledBitmap(bm, 200, 200, false);
 		profPic.setImageBitmap(resizedBM);
+	
+		//Set layout parameters of image view
+		/*MarginLayoutParams profPicMargin = new MarginLayoutParams(profPic.getLayoutParams());
+		profPicMargin.setMargins(0,profPic.getHeight()/2, 0, 0);		
+		RelativeLayout.LayoutParams profPicLP = new RelativeLayout.LayoutParams(profPicMargin);
+		profPic.setLayoutParams(profPicLP);*/
+		
+		//Set user name
+		userName.setText("Kyle Cooper");
 	}
 }
