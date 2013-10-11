a8f706424ead406b97bdc534f7353774b864ef0e
cooper3330 <kyle.cooper@uky.edu>
2013-10-11 01:17:38 -0400

    Sliding Menu implemented. Global button style created.

--- src/com/llc/bumpr/SearchDrivers.java
+++ src/com/llc/bumpr/SearchDrivers.java
@@ -12,12 +12,16 @@ import android.app.Activity;
 import android.os.Bundle;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
+
+import android.view.KeyEvent;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 public class SearchDrivers extends SherlockActivity {
 
 	com.actionbarsherlock.app.ActionBar actionBar;
 	private static final int RQS_GooglePlayServices = 1;
+	private SlidingMenu slidingMenu;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +30,50 @@ public class SearchDrivers extends SherlockActivity {
 		setContentView(R.layout.search_driver);
 		actionBar = getSupportActionBar();
 		
-		SlidingMenu menu = new SlidingMenu(this);		
+		//Set up sliding menu
+		slidingMenu = new SlidingMenu(this);
+		slidingMenu.setMode(SlidingMenu.LEFT);
+		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+		slidingMenu.setShadowWidthRes(R.dimen.slidingmenu_shadow_width);
+		slidingMenu.setShadowDrawable(R.drawable.shadow);
+		slidingMenu.setBehindOffsetRes(R.dimen.slidingmenu_offset);
+		slidingMenu.setFadeDegree(0.35f);
+		slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
+		slidingMenu.setMenu(R.layout.sliding_menu);
+		
+		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
+	
+	//If slidingMenu showing, back closes menu. Otherwise, calls parent back action
+	@Override
+	public void onBackPressed(){
+		if(slidingMenu.isMenuShowing())
+			slidingMenu.toggle();
+		else
+			super.onBackPressed();
+	}
+	
+	//If menu button pressed, show or hide the sliding menu
+	@Override
+	public boolean onKeyDown(int keyCode, KeyEvent event){
+		if (keyCode == KeyEvent.KEYCODE_MENU){
+			this.slidingMenu.toggle();
+			return true;
+		}
+		return super.onKeyDown(keyCode, event);
+	}
+	
+	//to show/hide the SlidingMenu when the home icon on the actionbar is pressed
+	/*@Override
+	public boolean onOptionsItemSelected(MenuItem item){
+		switch(item.getItemId()){
+			case android.R.id.home:
+				this.slidingMenu.toggle();
+				return true;
+			default:
+				return super.onOptionsItemSelected(item);
+		}
+	}*/
 	
     @Override
 	protected void onResume() {
