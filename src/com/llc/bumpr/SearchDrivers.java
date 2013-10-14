8a9c7b379d1be2b9bfd47272041bb9e3b7a6ef10
cooper3330 <kyle.cooper@uky.edu>
2013-10-13 22:51:59 -0400

    Map/ListView size changing done (split screen in half vertically) Search Bar always activated Default text in search bar Updated App Name (In @Strings) to lowercase version

--- src/com/llc/bumpr/SearchDrivers.java
+++ src/com/llc/bumpr/SearchDrivers.java
@@ -1,20 +1,34 @@
 package com.llc.bumpr;
 
+import java.util.logging.Logger;
+
 import com.jeremyfeinstein.slidingmenu.lib.SlidingMenu;
 import com.jeremyfeinstein.slidingmenu.lib.app.SlidingActivity;
 import com.llc.bumpr.R;
 import com.actionbarsherlock.app.SherlockActivity;
+import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 import com.google.android.gms.common.ConnectionResult;
 
 import com.actionbarsherlock.app.ActionBar;
 import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import com.actionbarsherlock.view.Menu;
+import com.actionbarsherlock.view.MenuItem;
 import com.actionbarsherlock.view.MenuInflater;
+import com.actionbarsherlock.widget.SearchView;
 
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.util.Log;
 import android.view.KeyEvent;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.Toast;
 
 public class SearchDrivers extends SherlockActivity {
@@ -22,6 +36,8 @@ public class SearchDrivers extends SherlockActivity {
 	com.actionbarsherlock.app.ActionBar actionBar;
 	private static final int RQS_GooglePlayServices = 1;
 	private SlidingMenu slidingMenu;
+	private LinearLayout map;
+	private LinearLayout driverLayout;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -29,8 +45,10 @@ public class SearchDrivers extends SherlockActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.search_driver);
 		actionBar = getSupportActionBar();
-		
-		//Set up sliding menu
+		map = (LinearLayout) findViewById(R.id.ll_map_container);
+		driverLayout = (LinearLayout) findViewById(R.id.ll_driver_list);
+
+		// Set up sliding menu
 		slidingMenu = new SlidingMenu(this);
 		slidingMenu.setMode(SlidingMenu.LEFT);
 		slidingMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
@@ -40,65 +58,106 @@ public class SearchDrivers extends SherlockActivity {
 		slidingMenu.setFadeDegree(0.35f);
 		slidingMenu.attachToActivity(this, SlidingMenu.SLIDING_WINDOW);
 		slidingMenu.setMenu(R.layout.sliding_menu);
-		
+
 		getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 	}
-	
-	//If slidingMenu showing, back closes menu. Otherwise, calls parent back action
+
+	// If slidingMenu showing, back closes menu. Otherwise, calls parent back
+	// action
 	@Override
-	public void onBackPressed(){
-		if(slidingMenu.isMenuShowing())
+	public void onBackPressed() {
+		if (slidingMenu.isMenuShowing())
 			slidingMenu.toggle();
+		else if (((LinearLayout.LayoutParams)map.getLayoutParams()).weight == .5){
+			//Set Driver List weight
+			LinearLayout.LayoutParams driverPars = (LinearLayout.LayoutParams)driverLayout.getLayoutParams();
+			driverPars.weight = 0.0f;
+			driverLayout.setLayoutParams(driverPars);
+			LinearLayout.LayoutParams mapPars = (LinearLayout.LayoutParams)map.getLayoutParams();
+			mapPars.weight = 1.0f;
+			map.setLayoutParams(mapPars);
+		}
 		else
 			super.onBackPressed();
 	}
-	
-	//If menu button pressed, show or hide the sliding menu
+
+	// If menu button pressed, show or hide the sliding menu
 	@Override
-	public boolean onKeyDown(int keyCode, KeyEvent event){
-		if (keyCode == KeyEvent.KEYCODE_MENU){
+	public boolean onKeyDown(int keyCode, KeyEvent event) {
+		if (keyCode == KeyEvent.KEYCODE_MENU) {
 			this.slidingMenu.toggle();
 			return true;
 		}
 		return super.onKeyDown(keyCode, event);
 	}
-	
-	//to show/hide the SlidingMenu when the home icon on the actionbar is pressed
-	/*@Override
-	public boolean onOptionsItemSelected(MenuItem item){
-		switch(item.getItemId()){
-			case android.R.id.home:
-				this.slidingMenu.toggle();
-				return true;
-			default:
-				return super.onOptionsItemSelected(item);
+
+	// to show/hide the SlidingMenu when the home icon on the actionbar is
+	// pressed
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case android.R.id.home:
+			this.slidingMenu.toggle();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
 		}
-	}*/
-	
-    @Override
+	}
+
+	@Override
 	protected void onResume() {
 		// TODO Auto-generated method stub
 		super.onResume();
-		
-		//Verify user has good version of google play services. Necessary for maps
-		int retCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable
-				(getApplicationContext());
-		//If successful, carry on.  Otherwise, request them to download GP Services
+
+		// Verify user has good version of google play services. Necessary for
+		// maps
+		int retCode = GooglePlayServicesUtil
+				.isGooglePlayServicesAvailable(getApplicationContext());
+		// If successful, carry on. Otherwise, request them to download GP
+		// Services
 		if (retCode != ConnectionResult.SUCCESS)
-			GooglePlayServicesUtil.getErrorDialog(retCode, this, RQS_GooglePlayServices).show();
+			GooglePlayServicesUtil.getErrorDialog(retCode, this,
+					RQS_GooglePlayServices).show();
 	}
 
-    
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// TODO Auto-generated method stub
 		super.onCreateOptionsMenu(menu);
-		
+
 		MenuInflater inflater = getSupportMenuInflater();
 		inflater.inflate(R.menu.options_menu, menu);
-		
+
+		SearchView searchView = (SearchView) menu.findItem(R.id.it_search_bar).getActionView();
+		searchView.setQueryHint("Enter destination here...");
+		searchView.setIconifiedByDefault(false);
+
+		searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+			// Search Listener
+			@Override
+			public boolean onQueryTextSubmit(String query) {
+				// TODO Auto-generated method stub
+				LinearLayout.LayoutParams mapPars = (LinearLayout.LayoutParams)map.getLayoutParams();
+				mapPars.weight = 0.5f;
+				map.setLayoutParams(mapPars);
+				//Set Driver List weight
+				LinearLayout.LayoutParams driverPars = (LinearLayout.LayoutParams)driverLayout.getLayoutParams();
+				driverPars.weight = 0.5f;
+				driverLayout.setLayoutParams(driverPars);
+				return true;
+			}
+
+			@Override
+			public boolean onQueryTextChange(String newText) {
+				// TODO Auto-generated method stub
+				
+				return false;
+			}
+		});
+
 		return true;
-		
+
 	}
 
 }
