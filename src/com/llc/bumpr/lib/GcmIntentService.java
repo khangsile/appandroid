5e302ae51c18783d131809aa8374a071b6aa431a
cooper3330 <kyle.cooper@uky.edu>
2013-11-01 03:23:05 -0400

    Create Notification settings to vibrate, make sound, and flash blue upon receiving new message!

--- src/com/llc/bumpr/lib/GcmIntentService.java
+++ src/com/llc/bumpr/lib/GcmIntentService.java
@@ -1,10 +1,12 @@
 package com.llc.bumpr.lib;
 
 import android.app.IntentService;
+import android.app.Notification;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.os.SystemClock;
 import android.support.v4.app.NotificationCompat;
@@ -91,7 +93,10 @@ public class GcmIntentService extends IntentService {
         .setStyle(new NotificationCompat.BigTextStyle()
         .bigText(msg))
         .setContentText(msg)
-        .setAutoCancel(true);
+        .setAutoCancel(true)
+        .setOnlyAlertOnce(true)
+        .setDefaults(Notification.DEFAULT_SOUND|Notification.DEFAULT_VIBRATE) //Make phone notify user and vibrate
+        .setLights(Color.rgb(0,0,128),1000,4000); //Flash blue light for 1 second on and 4 seconds off
 
         mBuilder.setContentIntent(contentIntent);
         mNotificationManager.notify(NOTIFICATION_ID, mBuilder.build());
