da7627cb707a33ec77741e01156d405e43d4a620
Khang Le <khangsile@gmail.com>
2013-11-17 01:25:36 -0500

    implement facebook login

---
+++ src/com/llc/bumpr/facebook/LoginFragment.java
@@ -0,0 +1,20 @@
+package com.llc.bumpr.facebook;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.llc.bumpr.R;
+
+public class LoginFragment extends SherlockFragment {
+	
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View view = inflater.inflate(R.layout.main, container, false);
+
+		return view;
+	}
+}
